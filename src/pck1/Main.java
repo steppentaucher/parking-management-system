3b67434396969662174f61c2f2cec165de6e6537
johan <johan@Hugo>
2026-05-20 15:26:14 +0200

    Name ausgegeben Johannes

--- src/pck1/Main.java
+++ src/pck1/Main.java
@@ -9,7 +9,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Geiles Programm");
-
+		System.out.println("Johannes is da :)");
 	}
 
 }
