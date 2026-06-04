6d97b45f8c09f9b2e8796e647360205d1f86a6fb
johan <johan@Hugo.speedport.ip>
2026-06-04 22:14:36 +0200

    Habe eine Klammer im PLManganger gestezt und eine doppelte Methode entfernt.

--- src/controller/PlattformManager.java
+++ src/controller/PlattformManager.java
@@ -16,38 +16,34 @@ public class PlattformManager {
 		this.alleParkplaetze = new java.util.ArrayList<>();
 		this.alleBuchungen = new java.util.ArrayList<>();
 		this.alleNutzer = new java.util.ArrayList<>();
-    	this.aktuellerNutzer = null;
+		this.aktuellerNutzer = null;
 	}
 
 	public boolean verfuegbarkeitPruefen(Parkplatz p, LocalDateTime von, LocalDateTime bis) {
-	    int count = 0;
-	    for (Buchung b : alleBuchungen) {
-	        if (b.getParkplatz().getId().equals(p.getId())) {
-	            
-	            if (!(bis.isBefore(b.getVon()) || bis.isEqual(b.getVon()) || von.isAfter(b.getBis()) || von.isEqual(b.getBis()))) {
-	                count++;
-	            }
-	        }
-	    }
+		int count = 0;
+		for (Buchung b : alleBuchungen) {
+			if (b.getParkplatz().getId().equals(p.getId())) {
 
-	    return count < p.getGesamtKapazitaet();
+				if (!(bis.isBefore(b.getVon()) || bis.isEqual(b.getVon()) || von.isAfter(b.getBis())
+						|| von.isEqual(b.getBis()))) {
+					count++;
+				}
+			}
+		}
+
+		return count < p.getGesamtKapazitaet();
+
+	}
+
 	public double berechnePreis(Parkplatz p, LocalDateTime von, LocalDateTime bis) {
 		Buchung testBuchung = new Buchung("test", p, null, von, bis);
 		return testBuchung.berechnePreis();
 	}
 
-
-    public boolean verfuegbarkeitPruefen(Parkplatz p, LocalDateTime von, LocalDateTime bis) {
-    	// TODO: Implementierung
-    			return true;
-    }
-
-
-    public Buchung bucheParkplatz(Parkplatz p, LocalDateTime von, LocalDateTime bis) {
-    	// TODO: Implementierung
-        return null;
-    }
-
+	public Buchung bucheParkplatz(Parkplatz p, LocalDateTime von, LocalDateTime bis) {
+		// TODO: Implementierung
+		return null;
+	}
 
 	public User registriereNutzer(String name, String email, String typ) {
 		// TODO: Implementierung
@@ -72,7 +68,7 @@ public class PlattformManager {
 	}
 
 	public void addNutzer(User user) {
-		if (user != null) {						// Kleine Hilfsmethode zum testen 
+		if (user != null) { // Kleine Hilfsmethode zum testen
 			alleNutzer.add(user);
 		}
 	}
@@ -90,8 +86,19 @@ public class PlattformManager {
 		// Ruft FileIO auf
 	}
 
-	public List<Parkplatz> getAlleParkplaetze() { return alleParkplaetze; }
-	public List<Buchung> getAlleBuchungen() { return alleBuchungen; }
-	public List<User> getAlleNutzer() { return alleNutzer; }
-	public User getAktuellerNutzer() { return aktuellerNutzer; }
+	public List<Parkplatz> getAlleParkplaetze() {
+		return alleParkplaetze;
+	}
+
+	public List<Buchung> getAlleBuchungen() {
+		return alleBuchungen;
+	}
+
+	public List<User> getAlleNutzer() {
+		return alleNutzer;
+	}
+
+	public User getAktuellerNutzer() {
+		return aktuellerNutzer;
+	}
 }
--- src/tests/PlattformManagerTest.java
+++ src/tests/PlattformManagerTest.java
@@ -29,70 +29,13 @@ public class PlattformManagerTest {
         manager.getAlleParkplaetze().add(parkplatz1);
     }
 
-    public static void main(String[] args) {
-        PlattformManagerTest test = new PlattformManagerTest();
+   
 
-        System.out.println("--- PlattformManager Verfügbarkeitsprüfung ---");
-        
-        // TC-01
-        test.setUp();
-        System.out.println("Fall 1: Parkplatz ist frei -> " + test.check(
-            LocalDateTime.of(2026, 6, 1, 10, 0), 
-            LocalDateTime.of(2026, 6, 1, 12, 0)
-        ));
+    
 
-        // TC-03
-        test.setUp();
-        test.addBooking(10, 12);
-        System.out.println("Fall 2: Parkplatz voll belegt -> " + test.check(
-            LocalDateTime.of(2026, 6, 1, 10, 0), 
-            LocalDateTime.of(2026, 6, 1, 12, 0)
-        ));
+   
 
-        // TC-04
-        test.setUp();
-        test.addBooking(10, 12);
-        System.out.println("Fall 3: Überschneidung (11-13 Uhr) -> " + test.check(
-            LocalDateTime.of(2026, 6, 1, 11, 0), 
-            LocalDateTime.of(2026, 6, 1, 13, 0)
-        ));
-
-        // TC-05
-        test.setUp();
-        test.addBooking(10, 12);
-        System.out.println("Fall 4: Anschluss (ab 12 Uhr) -> " + test.check(
-            LocalDateTime.of(2026, 6, 1, 12, 0), 
-            LocalDateTime.of(2026, 6, 1, 14, 0)
-        ));
-    }
-
-    private void setUp() {
-        manager = new PlattformManager();
-        p1 = new Parkplatz("1", "Test", "Str", 1, 10.0);
-        manager.getAlleParkplaetze().add(p1);
-    }
-
-    private void addBooking(int startH, int endH) {
-        LocalDateTime von = LocalDateTime.of(2026, 6, 1, startH, 0);
-        LocalDateTime bis = LocalDateTime.of(2026, 6, 1, endH, 0);
-        manager.getAlleBuchungen().add(new Buchung("B", p1, null, von, bis));
-
-        test.setUp();
-        test.testLoginMitGueltigerEmail();
-
-        test.setUp();
-        test.testLoginMitUngueltigerEmail();
-
-        test.setUp();
-     
-    }
-
-    void setUp() {
-        manager = new PlattformManager();
-        manager.addNutzer(new Kunde("k1", "Max Mustermann", "max@mail.de"));
-        manager.addNutzer(new Betreiber("b1", "Anna Betreiber", "anna@mail.de"));
-        // TODO: Hier später weitere Testdaten (Parkplatz, Buchung) initialisieren
-    }
+    
 
     void testLoginMitGueltigerEmail() {
         boolean ergebnis = manager.login("max@mail.de");
@@ -171,6 +114,8 @@ public class PlattformManagerTest {
 
     private boolean check(LocalDateTime von, LocalDateTime bis) {
         return manager.verfuegbarkeitPruefen(p1, von, bis);
+        
+    }
     void testStornierung_Freigabe() {
         System.out.println("testStornierung_Freigabe: noch nicht implementiert");
     }
